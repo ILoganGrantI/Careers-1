92b62264831d36aa16e7f4f664093c050cd43c33
Logan Grant <logan.grant1@ocsbstudent.ca>
2021-03-17 12:41:26 +0000

    -

---
+++ script.js
@@ -0,0 +1,29 @@
+document.getElementById('b1').addEventListener('click', change)
+
+function change () {
+	document.getElementById('paragraph').innerHTML = '1'
+}
+
+document.getElementById('b2').addEventListener('click', change1)
+
+function change1 () {
+	document.getElementById('paragraph').innerHTML = '2'
+}
+
+document.getElementById('b3').addEventListener('click', change2)
+
+function change2 () {
+	document.getElementById('paragraph').innerHTML = '3'
+}
+
+document.getElementById('b4').addEventListener('click', change3)
+
+function change3 () {
+	document.getElementById('paragraph').innerHTML = '4'
+}
+
+document.getElementById('b5').addEventListener('click', change4)
+
+function change4 () {
+	document.getElementById('paragraph').innerHTML = '5'
+}
